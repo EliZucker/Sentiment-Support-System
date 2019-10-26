9d9e313f614c35498656305fd0a215c64251d269
Nathan Tung <tungsten250@gmail.com>
2019-10-26 18:18:44 -0400

    twitter ready for use

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from twitterscraper import query_tweets
+from twitterscraper.query import query_user_info
 import datetime
 from publisher import g_pub
 #from util import setup_credentials
@@ -13,6 +14,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 publisher = g_pub()
+
+def selenium_query(domain_name, next_button_xpath, class_for_reviews):
+    pass
 
 def tripadvisor_query():
     browser = webdriver.Chrome()
@@ -39,7 +43,7 @@ def tripadvisor_query():
         browser.refresh()
     browser.close()
 
-tripadvisor_query()
+#tripadvisor_query()
 
 #input starting and ending datetime objects for range and any search terms (as one string). If we wanted
 #all jetblue query strings would just be "jetblue" if we wanted cancellations query strings would be
@@ -50,14 +54,18 @@ def twitter_query(start_date, end_date, query_strings="jetblue"):
         start_string = str(start_date.year) + "-" + str(start_date.month) + "-" + str(start_date.day)
         start_date += datetime.timedelta(days=1)
         end_string = str(start_date.year) + "-" + str(start_date.month) + "-" + str(start_date.day)
-        print(start_string + " | " + end_string)
-        #tweets = query_tweets(query_strings + " until:" + end_string + " since:" + start_string)
-        #for tweet in tweets:
-        #print(tweets[0].text + " | " + type(tweets[0].timestamp))
-            #publisher.send_customer_message(query_strings + "_twitter", ***REMOVED***'sentiment': tweet.text***REMOVED***, tweet.timestamp, post_id=)
+        tweets = query_tweets(query_strings + " until:" + end_string + " since:" + start_string)
+        print(str(tweets[0].username) + " | " + str(tweets[0].timestamp))
+        for tweet in tweets:
+            location = query_user_info(user=tweet.username).location
+            publisher.send_customer_message(query_strings + "_twitter", ***REMOVED***'sentiment': tweet.text, 'location': location***REMOVED***, tweet.timestamp)
+        # for tweet in tweets:
+
+    # start_string = str(start_date.year) + "-" + str(start_date.month) + "-" + str(start_date.day)
+    # end_string = str(end_date.year) + "-" + str(end_date.month) + "-" + str(end_date.day)
 
 
-#twitter_query(datetime.date(2019,3,30), datetime.date(2019,4,15))
+#twitter_query(datetime.date(2019,10,1), datetime.date(2019,10,2))
 
 # credential_path = "YHack-2019-a87246a5ff6d.json"
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
