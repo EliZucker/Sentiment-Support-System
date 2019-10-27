3b545d5929f1718175b167e2592f348e6de6c54f
Nathan Tung <tungsten250@gmail.com>
2019-10-27 00:27:15 -0400

    add pip upgrade to gcloud

--- scraper.py
+++ scraper.py
@@ -23,6 +23,7 @@ def tripadvisor_query(airline_differentiator, airline):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.get("https://www.tripadvisor.com/Airline_Review-" + airline_differentiator + "-Reviews-JetBlue#REVIEWS")
     for i in range(5, 175, 5):
