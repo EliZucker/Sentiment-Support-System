c0ba5964f93b8175d146dc45561a3888f79fdedf
alexreuter <areuter@lsoc.org>
2019-10-26 21:28:01 -0400

    chrome args

--- scrapedelays.py
+++ scrapedelays.py
@@ -121,12 +121,12 @@ def signal_handler(sig, frame):
 
 	
 signal.signal(signal.SIGINT, signal_handler)
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-gpu')
-# driver = webdriver.Chrome(chrome_options=chrome_options)
-driver = webdriver.PhantomJS()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver = webdriver.PhantomJS()
 driver.set_window_size(1120, 550)
 login()
 retries = 0
